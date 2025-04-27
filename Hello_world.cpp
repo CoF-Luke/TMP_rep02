864710734a9da2d0c0b697ec589e0ae5f5e4c2dc
CoF-Luke <vladpilot.new@gmail.com>
2025-04-27 21:49:55 +0300

    discard namespace std in Hello_world.cpp in patch1

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 #include<string>
 
-using namespace std;
-
 int main()
 {
-    string name;
-    
-    cout << "Write user name: " << endl;
-    cin >> name;
+    std::string name;
 
-    cout << "Hello world from " << name << endl;
+    std::cout << "Write user name: " << std::endl;
+    std::cin >> name;
+
+    std::cout << "Hello world from " << name << std::endl;
 }
